8a47b1720b6ec0d41e0dc649d1740f83a9ec3ab1
Junaid Hassan <123434178+JunaidHassan19@users.noreply.github.com>
2024-06-13 23:38:42 +0530

    Add files via upload

---
+++ 1. Varible & Data Types  -JS/first.js
@@ -0,0 +1,124 @@
+// console.log("Welcome to my Js Code");
+// console.log("Hey my Name is Junaid Hassan");
+// console.log("This is Js. ");
+
+// // -----------------------------------------------------------------
+// // (= is called assignment operator in programming)
+
+// name = "junaid hassan";
+// console.log(name);
+
+// age = 18;
+// console.log(age);
+
+// price = 199.99;
+// console.log(price);
+
+// // x = null;
+// // console.log(x);
+
+// y = undefined;
+// console.log(y);
+
+// isFollow = false;
+// console.log("isFollow");
+
+// // isFollow = True;
+// // console.log("isFollow");
+
+// radius = 15;
+// console.log(radius);
+
+// // ------------------------------------------------------------------
+// // use of let keywords
+// let fullName = "junaid hassan";
+// console.log(fullName);
+
+// let Age = 19;
+// console.log(Age);
+
+// // ----------------------------------------------------------------------
+// // use of var keyword
+// var fullname = "junaid hassan";
+// console.log(fullname);
+
+// // -----------------------------------------------------------------------
+// // use of const
+// const PI = 3.14;
+// console.log(PI);
+
+// // --------------------------------------------------------------------
+// // data types in Js
+// // Number-----1
+// {
+//     let age = 18;  
+//     console.log(age);
+// }
+// {
+//     let price = 200;
+//     console.log(price);
+// }
+
+// // string-----2
+// {
+//     let name = " junaid hassan";
+//     console.log(name);
+// }
+
+// // boolean------3
+// {
+//     isFollow = true;
+//     console.log(isFollow);
+// }
+
+// // undefined------4
+// {
+// //    let x;
+// }
+
+// // null-----5
+// let x = null;
+
+// // bigInt-----6
+// let x = BigInt("123");
+
+// // symbol-----7
+// let y = Symbol("Hello!");
+
+// // ----------------------------------------------------------------------------
+// const student = {
+//     name : "junaid hassan" ,
+//     age : 18 ,
+//     cgpa : 9.2,
+//     IsPass : true
+// };
+// console.log(student.IsPass);
+// // or
+// console.log(student["age"]);
+// // ------------------------------------------------------------------
+// //  this is how we make changes in code 
+// student["age"] = student["age"] + 1;
+// console.log(student.age);
+
+// student["name"] = "junni" ;
+// console.log(student.name);
+
+// // ----------------------------------------------------------------
+// const product = {
+//     name : "Ball Pen",
+//     rating : 4,
+//     offer : 5,
+//     price : 220
+// };
+// console.log(product);
+
+// -----------------------------------------------------------------
+const myProfile = {
+    username : "offx.juna1d" ,
+    followers : "13K" ,
+    following : 22 ,
+    IsFollow : true,
+    posts : 1,
+    bio : "This is my instagram profile"
+};
+console.log(myProfile);
